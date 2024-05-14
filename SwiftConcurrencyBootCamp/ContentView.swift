6506d90a708ba458c0ecdb0e4ebfcf2c8b648891
AhmedDana60 <ahmeddanamo@gmail.com>
2024-05-14 09:44:59 +0300

    Initial Commit

---
+++ SwiftConcurrencyBootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftConcurrencyBootCamp
+//
+//  Created by Ahmed Dana Mohammed on 5/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftConcurrencyBootCamp/SwiftConcurrencyBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootCampApp.swift
+//  SwiftConcurrencyBootCamp
+//
+//  Created by Ahmed Dana Mohammed on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
