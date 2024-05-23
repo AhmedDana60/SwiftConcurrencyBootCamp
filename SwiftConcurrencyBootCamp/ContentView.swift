6dc24cf5f9cb202183c5c0e1fcc395c60ce7a101
AhmedDana60 <ahmeddanamo@gmail.com>
2024-05-23 10:06:18 +0300

    [Clean] Update Docs

--- SwiftConcurrencyBootCamp/ContentView.swift
+++ SwiftConcurrencyBootCamp/ContentView.swift
@@ -15,7 +15,7 @@
  Merge = Joining two different branch
  Rebase = Moving one branch on top of another branch
  Cherry picking = Duplication (copying) one commit from one branch to another
- 
+ Pull Request (PR) = Request to merge branch
  
  COMMIT MESSAGES
  
